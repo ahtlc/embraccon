bd34f6bea271c6be98f5e51e87793a04cdfc69d9
Eder Marques <eder.marques@citi.org.br>
2019-04-05 14:57:19 -0300

    read data from file

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-alert('yay');
+const gel = selector => document.querySelector(selector);
+
+const convertFile = (e) => {
+  const file = e.target.files[0];
+  return new Promise((resolve) => {
+    const reader = new FileReader();
+    reader.onload = () => {
+      resolve(reader.result);
+    };
+    reader.readAsText(file);
+  });
+};
+
+gel('#convert').addEventListener('change', (e) => {
+  convertFile(e).then((data) => {
+    console.log(data);
+  });
+});
